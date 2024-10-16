7f7a3d99a2590e7b728988572caa9d60d2f73ee3
Erick Flores <126894236+ErickJuniorFloresLizarbe@users.noreply.github.com>
2024-10-16 17:05:04 -0500

    Update AistudioApplicationTests.java

--- src/test/java/pe/edu/vallegrande/aistudio/AistudioApplicationTests.java
+++ src/test/java/pe/edu/vallegrande/aistudio/AistudioApplicationTests.java
@@ -2,12 +2,15 @@ package pe.edu.vallegrande.aistudio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @SpringBootTest
 class AistudioApplicationTests {
 
-	@Test
+  @Test
 	void contextLoads() {
+	    log.info("default test");
 	}
 
 }
